482b9c49e970878c4702436c955dfe9fea788636
KSARIYA <ksariya1511@gmail.com>
2025-01-18 17:20:45 +0800

    First commit

---
+++ FrontEndWebboilerplate-main/something/script.js
@@ -0,0 +1,5 @@
+// Add interactivity (optional)
+document.querySelector('.contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('Thank you for your message! We will get back to you soon.');
+});
